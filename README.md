6edf6a5f430e52823fa6a8562d00271ebfd1223e
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2023-05-17 20:19:43 +0200

    Updated readme. Added build scripts.

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Total Pro Menu
+<h1 align="center">
+Total Pro Menu
+</h1>
+<p align="center">
 JavaScript/ES8 menu library.
-v 0.8.0
+<p>
+<p align="center">
+v0.8.0
+<p>
+
+[![build](https://github.com/dariuszdawidowski/total-pro-menu/actions/workflows/build.yml/badge.svg)](https://github.com/dariuszdawidowski/total-pro-menu/actions/workflows/build.yml)
+[![npm](https://img.shields.io/npm/v/total-pro-menu)](https://www.npmjs.com/package/total-pro-menu)
+[![license](https://img.shields.io/github/license/dariuszdawidowski/total-pro-menu?color=9cf)](./LICENSE)
+
+# About
+
+JavaScript/ES8 menu library.
+
+# Features
+
+- Vanilla JavaScript/ES8
+- No dependencies
+
+# Build minified bundle file
+
+```bash
+npm install
+npm run build
+```
+Note: This is browser-centric vanilla JavaScript library, npm is only used to minify and bundle files into one.
---
+++ build.js
@@ -0,0 +1,48 @@
+/**
+ * Build script v6
+ */
+
+const files = [
+    { src: 'total-pro-menu.css.ejs', dst: 'total-pro-menu.css' },
+    { src: 'total-pro-menu.js.ejs', dst: 'total-pro-menu.js' },
+];
+
+const fs = require('fs');
+const { readFile } = require('fs').promises;
+const ejs = require('ejs');
+const cleancss = require('clean-css');
+const { minify } = require('terser');
+
+const mincss = (filePath) => {
+    try {
+        const cssContent = fs.readFileSync(filePath, 'utf8');
+        const minifiedCss = new cleancss().minify(cssContent).styles;
+        return minifiedCss;
+    }
+    catch (error) {
+        console.error(`Error minifying css ${filePath}:`, error);
+        return null;
+    }
+};
+
+const minjs = async (filePath) => {
+    try {
+        const inputCode = await readFile(filePath, 'utf8');
+        const minifiedCode = (await minify(inputCode)).code;
+        return minifiedCode;
+    }
+    catch (error) {
+        console.error(`Error minifying js ${filePath}:`, error);
+        return null;
+    }
+};
+
+if (!fs.existsSync('dist')) fs.mkdirSync('dist');
+
+files.forEach((file) => {
+
+    ejs.render(fs.readFileSync(file.src, 'utf8'), { minjs, mincss }, {async: true})
+        .then(output => fs.writeFileSync('dist/' + file.dst, output, 'utf8'));
+
+});
+
